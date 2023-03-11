084215707fcd3de74a82b70a6e4e8c6f37f168bf
archydragon <mendor@yuuzukiyo.net>
2023-03-11 13:41:41 +0200

    Simple C++ program

--- main.cpp
+++ main.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 #include <janet.h>
 
+static Janet cfun_myinc(int32_t argc, Janet *argv) {
+    janet_fixarity(argc, 2);
+
+    int base = janet_getinteger(argv, 0);
+    int n = janet_getinteger(argv, 1);
+
+    return janet_wrap_integer(base + n);
+}
+
 int main() {
-    std::cout << "Hello from C++!\n";
+    std::cout << "[c++] init\n";
+    int a = 0;
+    std::cout << "[c++] a = " << a <<"\n";
 
-    // Initialize the virtual machine. Do this before any calls to Janet functions.
     janet_init();
-
-    // Get the core janet environment. This contains all of the C functions in the core
-    // as well as the code in src/boot/boot.janet.
     JanetTable *env = janet_core_env(nullptr);
+    static const JanetReg cfuns[] = {{"myinc", cfun_myinc, ""}};
+    janet_cfuns(env, "my", cfuns);
 
-    // One of several ways to begin the Janet vm.
-    janet_dostring(env, "(print `Hello from Janet!`)", "main", nullptr);
+    janet_dostring(env, "(print `[janet] init`)", "main", nullptr);
 
-    // Use this to free all resources allocated by Janet.
+    janet_def(env, "a", janet_wrap_integer(a), "");
+    Janet ret;
+    janet_dostring(env, "(def b (myinc a 5)) (print `[janet] a = ` a ` b = ` b) b", "main", &ret);
+    if (janet_checktype(ret, JANET_NUMBER)) {
+        int b = janet_unwrap_integer(ret);
+        std::cout << "[c++] returned b = " << b << "\n";
+    }
+
     janet_deinit();
 
     return 0;
